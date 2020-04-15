f52d67f16cf566eac6050386370daededd87df25
sandycamilo <git@github.com:sandycamilo/BEW-Contractor-Project.git>
2020-04-14 21:33:57 -0700

    palindromes.py

--- Code/all-starter-code/palindromes.py
+++ Code/all-starter-code/palindromes.py
@@ -13,23 +13,38 @@ def is_palindrome(text):
     # implement is_palindrome_iterative and is_palindrome_recursive below, then
     # change this to call your implementation to verify it passes all tests
     assert isinstance(text, str), 'input is not a string: {}'.format(text)
-    return is_palindrome_iterative(text)
-    # return is_palindrome_recursive(text)
+    # return is_palindrome_iterative(text)
+    return is_palindrome_recursive(text)
 
 
 def is_palindrome_iterative(text):
     # TODO: implement the is_palindrome function iteratively here
-    pass
+    left_index = 0 
+    right_index = len(text) -1 
+    while left_index <= right_index:
+        if text[left_index] != text[right_index]:
+            return False
+        left_index +=1 
+        right_index -=1
+    return True
     # once implemented, change is_palindrome to call is_palindrome_iterative
     # to verify that your iterative implementation passes all tests
 
+print(is_palindrome_iterative("tacocat"))
+print(is_palindrome_iterative("deed"))
+print(is_palindrome_iterative("asd"))
 
 def is_palindrome_recursive(text, left=None, right=None):
     # TODO: implement the is_palindrome function recursively here
-    pass
+    if text[left] != text[right]:
+        return False
+    if left == right or left > right:
+        return True
+    return is_palindrome_recursive(text, left +1, right -1)
     # once implemented, change is_palindrome to call is_palindrome_recursive
     # to verify that your iterative implementation passes all tests
-
+test_str = "tacocate"
+print(is_palindrome_recursive(test_str, 0, len(test_str)-1))
 
 def main():
     import sys
