4e38627e5957294b8979cae419e12f770ebf37a3
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2020-04-23 21:37:46 -0700

    classwork folder

---
+++ classwork/reverse_string.py
@@ -0,0 +1,17 @@
+# # function that will reverse a string using a stack
+
+def reverse_string(text):
+    my_stack = [] # set a stack using a list or array  
+                  # only push and pop to the end of the stack (top of the stack); it is more efficient because there's space there.
+
+    for letter in text: 
+        my_stack.append(letter) # push to the end
+
+    reversed_string = "" 
+    while len(my_stack) != 0: # while stack is not empty
+        reversed_string += my_stack.pop(-1) # contactenate each letter on the stack to the string ~ 
+                                        # the last letter pushed to stack is the first letter we can access now
+
+    return reversed_string
+
+print(reverse_string("abc"))
---
+++ classwork/test.py
@@ -0,0 +1,8 @@
+def compute_sum(n):
+    sum = n
+    for i in range(n):
+        sum += (i * 1)
+    return sum
+    
+print(compute_sum(4))
+
