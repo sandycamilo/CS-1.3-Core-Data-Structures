98c73e6de0fbfc564bae55ea06b2ab6f33ffe0de
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2020-04-29 22:43:05 -0700

    palindromes tests passing

--- Code/all-starter-code/palindromes.py
+++ Code/all-starter-code/palindromes.py
@@ -7,6 +7,7 @@ import string
 # string.ascii_letters is ascii_lowercase + ascii_uppercase
 
 
+
 def is_palindrome(text):
     """A string of characters is a palindrome if it reads the same forwards and
     backwards, ignoring punctuation, whitespace, and letter casing."""
@@ -14,37 +15,74 @@ def is_palindrome(text):
     # change this to call your implementation to verify it passes all tests
     assert isinstance(text, str), 'input is not a string: {}'.format(text)
     # return is_palindrome_iterative(text)
-    return is_palindrome_recursive(text)
-
+    return is_palindrome_recursive(text, 0, len(text) - 1)
 
 def is_palindrome_iterative(text):
     # TODO: implement the is_palindrome function iteratively here
-    left_index = 0 
-    right_index = len(text) -1 
-    while left_index <= right_index:
-        if text[left_index] != text[right_index]:
+
+    text = text.lower()
+    new_text = ""
+    for character in text:
+        if character in string.ascii_letters:
+            new_text += character
+    print(new_text) 
+
+    if text == "":
+        return True
+
+    left = 0 
+    right = len(text) -1 
+ 
+    while left <= right:
+        while not text[left].isalnum():
+            left += 1
+        while not text[right].isalnum():
+            right -= 1
+        if text[left] != text[right]:
             return False
-        left_index +=1 
-        right_index -=1
+        left +=1 
+        right -=1
     return True
+    
     # once implemented, change is_palindrome to call is_palindrome_iterative
     # to verify that your iterative implementation passes all tests
 
-print(is_palindrome_iterative("tacocat"))
-print(is_palindrome_iterative("deed"))
-print(is_palindrome_iterative("asd"))
+# print(is_palindrome_iterative("tacocat"))
+# print(is_palindrome_iterative("deed"))
+# print(is_palindrome_iterative("asd"))
 
 def is_palindrome_recursive(text, left=None, right=None):
     # TODO: implement the is_palindrome function recursively here
-    if text[left] != text[right]:
-        return False
+
+    text = text.lower()
+    new_text = ""
+    for character in text:
+        if character in string.ascii_letters:
+            new_text += character
+    print(new_text) 
+
+    if text == "":
+        return True
+
+    if left is None:
+        left = 0
+        right = len(text) - 1
+
+    while not text[left].isalnum():
+        left += 1
+    while not text[right].isalnum():
+        right -= 1
+
     if left == right or left > right:
         return True
-    return is_palindrome_recursive(text, left +1, right -1)
+    if text[left] != text[right]:
+        return False
+    return is_palindrome_recursive(text, left+1, right-1)
+    
     # once implemented, change is_palindrome to call is_palindrome_recursive
     # to verify that your iterative implementation passes all tests
-test_str = "tacocate"
-print(is_palindrome_recursive(test_str, 0, len(test_str)-1))
+# test_str = "tacocate"
+# print(is_palindrome_recursive(test_str, 0, len(test_str)-1))
 
 def main():
     import sys
